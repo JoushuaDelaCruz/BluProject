db4cec38e72c3a99c8646531a52f7948671cba24
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-04-21 21:43:34 -0700

    Added a footer on the homepage

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import { FaInstagram, FaFacebook, FaTwitter, FaDiscord } from "react-icons/fa";
+
+const Footer = () => {
+  const getYear = () => {
+    let today = new Date();
+    return today.getFullYear();
+  };
+  /* Still need to do the discord! */
+  return (
+    <footer className="footer">
+      <section className="footer-body">
+        <a
+          href="https://www.instagram.com/dcblu_creator/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram size="2.25rem" />
+        </a>
+        <a href="TODO" target="_blank" rel="noreferrer">
+          <FaDiscord size="2.25rem" />
+        </a>
+        <a
+          href="https://twitter.com/dcblu_creator"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTwitter size="2.25rem" />
+        </a>
+      </section>
+      <p className="footer-copyright">
+        DC Blu &copy; {getYear()} All Rights Reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
