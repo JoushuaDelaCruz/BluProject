d2f74cb8df3474204998fa19c39cfc3218dc8919
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-04-21 21:37:07 -0700

    Added nodemon and react icons

--- objects/Chapter.ts
+++ objects/Chapter.ts
@@ -2,4 +2,22 @@ class Chapter {
     private _id: number;
     private _title: string;
     private _content: string;
+
+    constructor(id: number, title: string, content: string) {
+        this._id = id;
+        this._title = title;
+        this._content = content;
+    }
+
+    public get id(): number {
+        return this._id;
+    }
+
+    public get title(): string {
+        return this._title;
+    }
+
+    public get content(): string {
+        return this._content;
+    }
 }
